4c3cd0f49f94bc3fcd4e2ec0e0c1b4ff7026cc6c
Kartik Patel <kartikpatel@fueledbysoda.com>
2021-01-31 17:49:42 -0500

    Reading PORT from environment, default to 8080

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,15 @@
+use std::env;
 use actix_web::{App, HttpServer};
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    let port = env::var("PORT").unwrap_or(String::from("8080"));
+    let addr = format!("127.0.0.1:{}", port); 
+
     HttpServer::new(|| {
         App::new()
     })
-    .bind("127.0.0.1:8080")?
+    .bind(addr)?
     .run()
     .await
 }
